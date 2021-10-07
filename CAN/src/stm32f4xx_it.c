a26a738366465939fd83eb5ff20723c52b92b9c0
Apurv Sarve <40910158+apurv1012@users.noreply.github.com>
2021-10-07 22:17:23 +0530

    Add files via upload

---
+++ CAN/src/stm32f4xx_it.c
@@ -0,0 +1,47 @@
+
+#include "main.h"
+
+extern CAN_HandleTypeDef hcan1;
+extern TIM_HandleTypeDef htimer6;
+extern void CAN1_Tx(uint8_t remote);
+
+
+void SysTick_Handler (void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+
+void CAN1_TX_IRQHandler(void)
+{
+	HAL_CAN_IRQHandler(&hcan1);
+}
+
+void CAN1_RX0_IRQHandler(void)
+{
+	HAL_CAN_IRQHandler(&hcan1);
+
+}
+
+void CAN1_SCE_IRQHandler(void)
+{
+	HAL_CAN_IRQHandler(&hcan1);
+}
+
+
+void TIM6_DAC_IRQHandler(void)
+{
+	HAL_TIM_IRQHandler(&htimer6);
+
+}
+
+
+void EXTI0_IRQHandler(void)
+{
+	HAL_TIM_Base_Start_IT(&htimer6);
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+
+}
+
+
