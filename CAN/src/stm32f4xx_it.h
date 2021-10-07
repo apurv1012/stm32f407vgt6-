0988fedf5170e46a2f6adaaf0496101e7250ead2
Apurv Sarve <40910158+apurv1012@users.noreply.github.com>
2021-10-07 22:16:04 +0530

    Delete stm32f4xx_it.h

--- CAN/src/stm32f4xx_it.h
+++
@@ -1,9 +0,0 @@
-/* USER CODE BEGIN Header */
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32F4xx_IT_H
-#define __STM32F4xx_IT_H
-
-
-#endif /* __STM32F4xx_IT_H */
-
-
